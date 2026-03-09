ef13134aa8dbe8a191b0fc0f4aab7180a38311b9
David Bista <ytd3809@gmail.com>
2026-03-09 16:56:25 +0545

    feat: add About component to App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About/About';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 
@@ -10,6 +11,8 @@ function App() {
       
       <Header />
       <Hero />
+
+      <About />
     </>
   )
 }
