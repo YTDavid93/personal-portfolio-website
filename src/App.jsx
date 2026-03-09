d0f39292f2c7a064a455021fbc5c5b04644c0e0e
David Bista <ytd3809@gmail.com>
2026-03-09 15:24:32 +0545

    feat: render Hero component inside App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import Header from './components/Header/Header'
-import './App.css'
+import './App.css';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="cursor-ring" id="cursorRing"></div>
       
       <Header />
+      <Hero />
     </>
   )
 }
