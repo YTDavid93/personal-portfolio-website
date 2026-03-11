6e7612c8859ac4c8f58c639475da83e415e0599a
David Bista <ytd3809@gmail.com>
2026-03-11 22:40:33 +0545

    feat: add contact and footer sections with cursor component to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import Experience from './components/Experience/Experience';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Project from './components/Project/Project';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Cursor from './components/Cursor/Cursor';
+import { useScrollReveal } from './hooks/userScrollReveal';
 
 function App() {
+  useScrollReveal();
+
   return (
     <>
-      <div className="cursor" id="cursor"></div>
-      <div className="cursor-ring" id="cursorRing"></div>
-      
+      {/* render cursor component  */}
+      <Cursor />
       {/* render header section */}
       <Header />
       {/* render hero section */}
@@ -21,6 +26,10 @@ function App() {
       <Project />
       {/* render experience section */}
       <Experience />
+      {/* render contact section */}
+      <Contact />
+      {/* render footer section */}
+      <Footer />
     </>
   )
 }
